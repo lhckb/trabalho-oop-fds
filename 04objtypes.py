cf1cbc666f8db990fb5bdef2680ca28b3501d7b7
lhckb <luis.carv.cruz@gmail.com>
2022-03-18 10:57:16 -0300

    add missing example

---
+++ 04objtypes.py
@@ -0,0 +1,15 @@
+class Human:
+    pass
+
+# definida e instanciada uma classe Human, vamos verificar
+# o tipo do objeto de Human
+luis = Human()
+print(type(luis))
+
+# paralelamente, vamos verificar o tipo de uma string
+str = 'oi mate, care for a tea?'
+print(type(str))
+
+# por fim, o tipo de um inteiro
+num = 1
+print(type(num))
